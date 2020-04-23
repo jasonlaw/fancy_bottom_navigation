2fc0ab27a50781a61d71f6ec90b090413adc5135
Jason Law <jason.cclaw@gmail.com>
2020-04-23 16:59:28 +0800

    right to left support

--- lib/fancy_bottom_navigation.dart
+++ lib/fancy_bottom_navigation.dart
@@ -175,7 +175,12 @@ class FancyBottomNavigationState extends State<FancyBottomNavigation>
             child: AnimatedAlign(
               duration: Duration(milliseconds: ANIM_DURATION),
               curve: Curves.easeOut,
-              alignment: Alignment(_circleAlignX, 1),
+              alignment: Alignment(
+                  _circleAlignX *
+                      (Directionality.of(context) == TextDirection.rtl
+                          ? -1
+                          : 1),
+                  1),
               child: Padding(
                 padding: const EdgeInsets.only(bottom: 14),
                 child: FractionallySizedBox(
